d411b5dae35b280aeccd537d807ae114f1437ce9
karlosmatos <105454484+karlosmatos@users.noreply.github.com>
2022-07-09 21:51:28 +0200

    Update tic_tac_toe(CZ).py

--- tic_tac_toe(CZ).py
+++ tic_tac_toe(CZ).py
@@ -6,31 +6,52 @@ def print_board(board):
         print(i)
 
 def hrac1_vstup(board, board_size, symbols):
-    hrac1 = list(map(int, input('Hraje hrac1. Zadej polohu symbolu v pořadí řádek-sloupec: ').split()))
-    if board[hrac1[0]-1][hrac1[1]-1] == '.':
-        board[hrac1[0]-1][hrac1[1]-1] = symbols[0]
-    else:
-        while board[hrac1[0]-1][hrac1[1]-1] != '.':
-            hrac1 = list(map(int, input('Toto pole už je vybrané nebo mimo mimo hrací prostředí. Zkus vybrat něco jiného: ').split()))
-        board[hrac1[0]-1][hrac1[1]-1] = symbols[0]    
+    hrac1 = list(map(int, input('Hraje Hráč č.1. Zadej pozici symbolu v pořadí řádek-sloupec: ').split()))
+    hrac1 = [i-1 for i in hrac1]
+    try:
+        if board[hrac1[0]][hrac1[1]] == '.':
+            board[hrac1[0]][hrac1[1]] = symbols[0]
+        else:
+            while board[hrac1[0]][hrac1[1]] != '.':
+                hrac1 = list(map(int, input('Toto pole je už vybrané nebo mimo hrací prostředí. Zkus vybrat něco jiného: ').split()))
+                hrac1 = [i-1 for i in hrac1]
+            board[hrac1[0]][hrac1[1]] = symbols[0]
+    except IndexError:
+        while IndexError or board[hrac2[0]][hrac2[1]] != '.':
+            hrac2 = list(map(int, input('Toto pole je už vybrané nebo mimo hrací prostředí. Zkus vybrat něco jiného: ').split()))
+            hrac2 = [i-1 for i in hrac2]
+        board[hrac2[0]][hrac2[1]] = symbols[1]
+    except:
+        while board[hrac1[0]][hrac1[1]] != '.':
+            hrac1 = list(map(int, input('Toto pole je už vybrané nebo mimo hrací prostředí. Zkus vybrat něco jiného: ').split()))
+            hrac1 = [i-1 for i in hrac1]
+        board[hrac1[0]][hrac1[1]] = symbols[0]    
     print_board(board)
     return board
 
 def hrac2_vstup(board, board_size, symbols, two_players):
     robot = []
     if two_players == 'ano':
-        hrac2 = list(map(int, input('Hraje hrac2. Zadej polohu symbolu v pořadí řádek-sloupec: ').split()))
+        hrac2 = list(map(int, input('Hraje Hráč č.1. Zadej pozici symbolu v pořadí řádek-sloupec: ').split()))
         hrac2 = [i-1 for i in hrac2]
-        if IndexError:
+        try:
+            if board[hrac2[0]][hrac2[1]] == '.':
+                board[hrac2[0]][hrac2[1]] = symbols[1]
+            else:
+                while board[hrac2[0]][hrac2[1]] != '.':
+                    hrac2 = list(map(int, input('Toto pole je už vybrané nebo mimo hrací prostředí. Zkus vybrat něco jiného: ').split()))
+                    hrac2 = [i-1 for i in hrac2]
+                board[hrac2[0]][hrac2[1]] = symbols[1]
+        except IndexError:
             while IndexError or board[hrac2[0]][hrac2[1]] != '.':
-                hrac2 = list(map(int, input('Toto pole už je vybrané. Zkus vybrat něco jiného: ').split()))
+                hrac2 = list(map(int, input('Toto pole je už vybrané nebo mimo hrací prostředí. Zkus vybrat něco jiného: ').split()))
+                hrac2 = [i-1 for i in hrac2]
             board[hrac2[0]][hrac2[1]] = symbols[1]
-        if board[hrac2[0]][hrac2[1]] == '.':
-            board[hrac2[0]][hrac2[1]] = symbols[1]
-        else:
+        except:
             while board[hrac2[0]][hrac2[1]] != '.':
-                hrac2 = list(map(int, input('Toto pole už je vybrané. Zkus vybrat něco jiného: ').split()))
-            board[hrac2[0]][hrac2[1]] = symbols[1]
+                hrac2 = list(map(int, input('Toto pole je už vybrané nebo mimo hrací prostředí. Zkus vybrat něco jiného: ').split()))
+                hrac2 = [i-1 for i in hrac2]
+            board[hrac2[0]][hrac2[1]] = symbols[1]    
         print_board(board)
         return board
     else:
@@ -47,18 +68,18 @@ def win(board, board_size, symbols, hra):
     hra = True
     for iradek in range(board_size[0]):
         for jsloupec in range(board_size[1]):
-            if board[iradek][jsloupec] == board[iradek][jsloupec-1] == board[iradek][jsloupec-2] and board[iradek][jsloupec] in symbols:
+            if board[iradek][jsloupec] == board[iradek][jsloupec-1] == board[iradek][jsloupec-2] and board[iradek][jsloupec] in symbols: #řádek
                 hra = False
                 symbol = board[iradek][jsloupec]
-            elif board[iradek][jsloupec] == board[iradek-1][jsloupec] == board[iradek-2][jsloupec] and board[iradek][jsloupec] in symbols:
+            elif board[iradek][jsloupec] == board[iradek-1][jsloupec] == board[iradek-2][jsloupec] and board[iradek][jsloupec] in symbols: #sloupec
                 hra = False
                 symbol = board[iradek][jsloupec]
-            elif board[iradek][jsloupec] == board[iradek-1][jsloupec-1] == board[iradek-2][jsloupec-2] and board[iradek][jsloupec] in symbols:
+            elif board[iradek][jsloupec] == board[iradek-1][jsloupec-1] == board[iradek-2][jsloupec-2] and board[iradek][jsloupec] in symbols: #diagonal zpravo doleva nahoru
                 hra = False
                 symbol = board[iradek][jsloupec]
-            elif board[iradek][jsloupec] == board[iradek-2][jsloupec-1] == board[iradek-1][jsloupec-2] and board[iradek][jsloupec] in symbols:
+            elif board[iradek-1][jsloupec-1] == board[iradek-2][jsloupec] == board[iradek][jsloupec-2] and board[iradek-1][jsloupec-1] in symbols: #diagonal zpravo doleva dolů
                 hra = False
-                symbol = board[iradek][jsloupec]
+                symbol = board[iradek-1][jsloupec-1]
     if hra == False:
         print(f'Vítězí hráč se symbolem: {symbol}. Gratulujeme!')
     return hra
