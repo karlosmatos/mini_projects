340278f1cabe8ed4f894178e6a87c3c461f1ddf9
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2022-07-13 22:37:48 +0200

    Update login_function.py

--- login_function.py
+++ login_function.py
@@ -11,18 +11,18 @@ user_password = [
 login = ''
 password = ''
 
-def log_in(user_login, user_password):
-    if login in username_login:
-        if password in username_password:
-            if uzivatele_login.index(login) == uzivatele_password.index(password): #check if the indexes in the given lists match
+def log_in(login, password, user_login, user_password):
+    if login in user_login:
+        if password in user_password:
+            if user_login.index(login) == user_password.index(password): #check if the indexes in the given lists match
                 print('You got in')
     else:
         print("Unfortunately, you don't have permission.")
-        novy_user = input('To register your account, type "Yes": ').lower() #query to register a new user
-        return novy_user
+        new_user = input('To register your account, type "Yes": ').lower() #query to register a new user
+        return new_user
 
 def add_user(login, password, user_login, user_password, new_user):
-    if novy_user == 'yes':
+    if new_user == 'yes':
         user_login.append(login)
         user_password.append(password)
         print('Your account has been successfully created :)')
@@ -30,9 +30,9 @@ def add_user(login, password, user_login, user_password, new_user):
 def main():
     login = input('Enter your login credentials: ')
     password = input('Enter your password: ')
-    new_user = log_in(user_login, user_password)
+    new_user = log_in(login, password, user_login, user_password)
     add_user(login, password, user_login, user_password, new_user)
-    print(username_login, username_password) #validate credit
+    print(user_login, user_password) #validate credit
 
 
 if __name__ == "__main__":
